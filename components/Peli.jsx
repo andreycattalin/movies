1531474acbd8d786be6ce3000d49d47835c92d6a
Andrey <cattalin.htc@gmail.com>
2023-10-09 12:32:53 +0200

    --wip-- [skip ci]

--- components/Peli.jsx
+++ components/Peli.jsx
@@ -1,6 +1,8 @@
+"use client"
 export default function Peli({ peli }) {
     return (
         <div className="bg-white shadow p-4 rounded-md m-1">
+            <img src={`https://image.tmdb.org/t/p/w500${peli.poster_path}`} />
             <h1 className="text-xl font-bold line-clamp-3">{peli.title}</h1>
             <p className="text-xs line-clamp-6">{peli.overview}</p>
         </div>
